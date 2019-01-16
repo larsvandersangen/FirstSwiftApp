071e27ee4062d26408adffee78ca6ec6b4365b8d
Lars van der Sangen <larsvandersangen@hotmail.com>
2019-01-16 18:06:04 +0100

    Initial commit. First IOS and SWIFT APP. Simple label changer button with background changer button.

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    
+    var colorArray: Array<UIColor> = [
+        UIColor.black,
+        UIColor.white,
+        UIColor.green,
+        UIColor.brown,
+        UIColor.gray
+    ]
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func changeBackgroundRandom(_ sender: Any)
+    {
+        // Get random UIColor from colorArray
+        let randomUIColor: UIColor = self.colorArray.randomElement()!
+        
+        // Set Random UIColor to view.background
+        self.view.backgroundColor = randomUIColor;
+    }
+    
+    @IBAction func clickButton(_ sender: Any)
+    {
+        self.helloWorldLabel.text = "The button has been clicked correctly"
+    }
 }
 
